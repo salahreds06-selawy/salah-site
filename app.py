151ffd0f55630fcd5546df64ae7d34592169bf24
slawi-salah <salah.reds.06@gmail.com>
2026-07-29 21:08:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return " موقعي يعمل على GitHub! "if name =
-
-= '__main__':    app.r
-un()
+    return " موقعي يعمل على GitHub! "
+    if __name__ == '__main__':  
+        app.run()
